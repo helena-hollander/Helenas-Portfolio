9e11e26a05a2dfd79458de34275cf6cf30dc0d06
helena-hollander <helena.hollander@gmail.com>
2023-09-01 15:20:19 +0200

    Helena done fredag. JS er lidt hårdt

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
+//Opret HTML element
+
+const body = document.body
+const div = document.createElement("div")
+div.innerText ="Virker du?"
+body.append(div)
 
 
-// const body = document.body
-// const div = document.querySelector("div")
+
 const testAbout = document.querySelector("#about")
 testAbout.style.backgroundColor = "red"
+
+//Tekststrenge
+
+//document.getElementById("Ny Titel")
+document.getElementById ("overskrift").textContent = "Ny Titel";
+
+
+
+
 
 
 
