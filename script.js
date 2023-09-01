d6b392ca644414d85efcf3513b819fa8428044d1
helena-hollander <helena.hollander@gmail.com>
2023-09-01 14:40:48 +0200

    "Helena leger med JS"

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+// const body = document.body
+// const div = document.querySelector("div")
+const testAbout = document.querySelector("#about")
+testAbout.style.backgroundColor = "red"
+
+
+
+
+
+
+
+
